341decd87978686438c16f5618cff383c63a1f27
Carlos Junior Armenta <93354352+carlosjunior2007@users.noreply.github.com>
2025-11-16 23:00:48 -0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+var config = {
+  lines: [
+    "Marili,",
+    "Gracias por estos días, meses y años contigo.",
+    "Eres la parte tranquila de mis días pesados.",
+    "Tu forma de ser me hace querer ser mejor.",
+    "No sé qué hice para merecerte, pero lo valoro.",
+    "Me haces feliz de una manera que no sabía que necesitaba.",
+    "Te amo.",
+  ],
+  date: "2022-12-05T05:12:00",
+  names: ["Junior", "Marili"],
+};
